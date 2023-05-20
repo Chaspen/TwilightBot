b4e5e69be45b7fc633c415b144343ec2006470a8
Chloe Ramos <chaspen27alt@gmail.com>
2023-05-19 22:36:45 -0400

    Add files via upload

---
+++ twilight.py
@@ -0,0 +1,22 @@
+from mastodon import Mastodon
+from apscheduler.schedulers.blocking import BlockingScheduler
+import os, random
+from datetime import datetime
+
+now = datetime.now()
+dt_string = now.strftime("%m/%d/%Y %H:%M:%S")
+
+mastodon = Mastodon(
+    access_token = 'token.secret',
+    api_base_url = 'https://botsin.space/'
+)
+
+def PostTwi():
+    random_file = random.choice(os.listdir("img/twi"))
+    media = mastodon.media_post('img/twi/' + random_file)
+    mastodon.status_post("!!New Twilight Image!!", media_ids=media)
+    print('posted a twi at ' + dt_string  + ' :3')
+    
+scheduler = BlockingScheduler()
+scheduler.add_job(PostTwi, 'interval', hours=1)
+scheduler.start()
